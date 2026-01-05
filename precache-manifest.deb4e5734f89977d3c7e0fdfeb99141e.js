849fbec112483ce8a0b1135e3167755d1a4396b8
vitorclaudio <vitorcldeosantos@gmail.com>
2026-01-05 18:13:45 -0300

    Deploy

---
+++ precache-manifest.deb4e5734f89977d3c7e0fdfeb99141e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b671e36a72bc6947fa30885a8f8b24e1",
+    "url": "/Bfour_store/index.html"
+  },
+  {
+    "revision": "3fa393934a9b6b565b16",
+    "url": "/Bfour_store/static/css/2.5fc0a91c.chunk.css"
+  },
+  {
+    "revision": "555b15e53bc6720de3f9",
+    "url": "/Bfour_store/static/css/main.22a414dd.chunk.css"
+  },
+  {
+    "revision": "3fa393934a9b6b565b16",
+    "url": "/Bfour_store/static/js/2.484b9b30.chunk.js"
+  },
+  {
+    "revision": "db94e03f5604d0bb4bc267fa4b4b74f0",
+    "url": "/Bfour_store/static/js/2.484b9b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555b15e53bc6720de3f9",
+    "url": "/Bfour_store/static/js/main.125dfb9b.chunk.js"
+  },
+  {
+    "revision": "6f5eec03a27772b6d2e9",
+    "url": "/Bfour_store/static/js/runtime-main.29b76f93.js"
+  },
+  {
+    "revision": "8b5a349caeca92f786dedb0b4385d8d3",
+    "url": "/Bfour_store/static/media/banner_4.8b5a349c.jpg"
+  },
+  {
+    "revision": "f2ff77f23f5de5ed7af34e6f4f459a2e",
+    "url": "/Bfour_store/static/media/banner_6.f2ff77f2.jpg"
+  },
+  {
+    "revision": "012d93bb220fdd4e92698ac8f66051c9",
+    "url": "/Bfour_store/static/media/banner_7.012d93bb.jpg"
+  },
+  {
+    "revision": "900a5aa990035102947713d373b823b1",
+    "url": "/Bfour_store/static/media/deal_ofthe_week7.900a5aa9.png"
+  },
+  {
+    "revision": "0280082048c8b0a72b8a7bfd582d114b",
+    "url": "/Bfour_store/static/media/emptyCart.02800820.png"
+  },
+  {
+    "revision": "abebef23e38854b0a7ad161380471493",
+    "url": "/Bfour_store/static/media/empty_cart.abebef23.png"
+  },
+  {
+    "revision": "ce37de1fd511d38dd0ecf33de372275f",
+    "url": "/Bfour_store/static/media/slider_4.ce37de1f.jpg"
+  },
+  {
+    "revision": "c0366fd00b80a9308f39d2ae854548b9",
+    "url": "/Bfour_store/static/media/slider_5.c0366fd0.jpg"
+  },
+  {
+    "revision": "68d2d4e1c3b8b6f35a5a93feae6d41e8",
+    "url": "/Bfour_store/static/media/slider_6.68d2d4e1.jpg"
+  }
+]);
